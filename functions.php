c7da9663c6cebf996f51b53d095d6260d5e9aeba
Sephsekla <dev@joebr.io>
2017-12-11 11:08:18 +0000

    Google Fonts
    Included Open Sans by default

--- functions.php
+++ functions.php
@@ -130,7 +130,7 @@ wp_enqueue_script( 'popper',  'https://cdnjs.cloudflare.com/ajax/libs/popper.js/
  add_action( 'wp_enqueue_scripts', 'bootstrap_scripts',2 );
 
 function ignition_scripts() {
-	wp_enqueue_style( 'ignition-style', get_stylesheet_uri() );
+
 
 	wp_enqueue_script( 'jquery-mobile', get_template_directory_uri() . '/lib/jquery.mobile.custom/jquery.mobile.custom.min.js', array('jquery'), '1.4.5', true );
 
@@ -146,7 +146,11 @@ function ignition_scripts() {
 
 	wp_enqueue_script( 'ignition-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+//Fonts
 
+wp_enqueue_script('fontawesome','https://use.fontawesome.com/300bc96cdf.js', '', '', true);
+
+wp_enqueue_style( 'google-font', '//fonts.googleapis.com/css?family=Open+Sans:300,400,700' );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -169,6 +173,7 @@ function ignition_scripts() {
 
 	}
 
+wp_enqueue_style( 'ignition-style', get_stylesheet_uri() );
 
 }
 add_action( 'wp_enqueue_scripts', 'ignition_scripts' );
