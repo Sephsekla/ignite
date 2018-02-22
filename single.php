f64ddf9f974f701b6b51baccce9c722e63c9b227
Sephsekla <dev@joebr.io>
2018-02-22 14:47:57 +0000

    New Filter
    post_nav - allows filter of the_post_navigation

--- single.php
+++ single.php
@@ -18,7 +18,7 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation();
+			echo apply_filters('post_nav',get_the_post_navigation());
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
